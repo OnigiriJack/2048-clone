5c9a51865beae8ce77ca7a372ce920b932708ceb
OnigiriJack <jackmihalik@icloud.com>
2021-01-30 22:59:03 +0900

    Test Case for swipe up

--- src/2048_logic/2048.test.js
+++ src/2048_logic/2048.test.js
@@ -66,3 +66,35 @@ test("Right swipe Case 2 ", () => {
     ])
 })
 
+
+test("Up Swipe Case 1 ", () => {
+    let grid = [
+        [0, 0, 2, 16],
+        [16, 2, 16, 0],
+        [0, 32, 0, 32],
+        [8, 4, 2, 2]
+    ]
+    expect(swipe(grid, "up")).toStrictEqual([
+        [16, 2, 2, 16],
+        [8, 32, 16, 32],
+        [0, 4, 0, 2],
+        [0, 8, 2, 0]
+    ])
+})
+
+
+test("Up swipe case 2 ", () => {
+    let grid = [
+        [4, 4, 4, 4],
+        [4, 2, 0, 2],
+        [0, 0, 4, 0],
+        [4, 4, 2, 2]
+    ]
+    expect(swipe(grid, "up")).toStrictEqual([
+        [8, 4, 8, 4],
+        [4, 2, 2, 4],
+        [4, 4, 0, 0],
+        [0, 0, 0, 0]
+    ])
+})
+
