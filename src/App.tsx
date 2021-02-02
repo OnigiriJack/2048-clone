593256f017561adc8b4d1244d517f1b183327e18
OnigiriJack <jackmihalik@icloud.com>
2021-02-02 20:24:59 +0900

    Trying to integrate keyboard detectin

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { swipe, createBoard } from "./2048_logic/2048_game"
 function App() {
-
   const [board, setBoard] = useState([[0]])
-  function move(direction: string) {
-    setBoard(swipe(board, direction))
+
+  function move(direction: any) {
+    console.log(direction.key)
+    let dir = "";
+    // if (direction === 38) dir = "up"
+    // if (direction === 40) dir = "down"
+    // if (direction === 37) dir = "left"
+    // if (direction === 39) dir = "right"
+    setBoard(swipe(board, dir))
   }
   useEffect(() => {
     let freshBoard = createBoard();
@@ -17,9 +23,11 @@ function App() {
   }, [])
   return (
     <div className="App">
+
+      <input onKeyPress={move} />
       <h1>hello world</h1>
       <Grid board={board} />
-      <button onClick={() => move("up")}>
+      {/* <button onClick={() => move("up")}>
         up
       </button>
       <button onClick={() => move("down")}>
@@ -30,7 +38,7 @@ function App() {
       </button>
       <button onClick={() => move("left")}>
         left
-      </button>
+      </button> */}
     </div>
   );
 }
