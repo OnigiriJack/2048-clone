d7458283df4dd663dc45dd21f05cf2f38e813426
OnigiriJack <jackmihalik@icloud.com>
2021-02-07 17:20:33 +0900

    Added types to grid.tsx

--- src/2048_grid.tsx
+++ src/2048_grid.tsx
@@ -1,17 +1,17 @@
 
-function Grid(props: any) {
+function Grid(board: number[][]) {
     function colorSelector(num: number) {
         if (num == 2) return "#81591c";
-        if (num == 4) return "#d1c4e9"
-        if (num == 8) return "#7e57c2"
-        if (num == 16) return "#512da8"
-        if (num == 32) return "#311b92"
-        if (num == 64) return "#2196f3"
-        if (num == 128) return "#0d47a1"
-        if (num == 256) return "#80cbc4"
-        if (num == 512) return "#cddc39"
-        if (num == 1024) return "grey"
-        if (num == 2048) return "gold"
+        if (num == 4) return "#d1c4e9";
+        if (num == 8) return "#7e57c2";
+        if (num == 16) return "#512da8";
+        if (num == 32) return "#311b92";
+        if (num == 64) return "#2196f3";
+        if (num == 128) return "#0d47a1";
+        if (num == 256) return "#80cbc4";
+        if (num == 512) return "#cddc39";
+        if (num == 1024) return "grey";
+        if (num == 2048) return "gold";
     }
 
 
@@ -19,8 +19,7 @@ function Grid(props: any) {
         <div className="board">
             <table>
                 <tbody>
-                    {props.board.map((row: any, index: number) => {
-                        let className = "two";
+                    {board.map((row: number[], index: number) => {
                         return (
                             <tr>
                                 <td style={{ backgroundColor: colorSelector(row[0]) }}>{row[0]}</td>
