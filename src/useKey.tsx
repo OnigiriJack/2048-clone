17448e29e6da8d5c116de1b6c6764c99fd124dd9
OnigiriJack <jackmihalik@icloud.com>
2021-02-11 10:35:52 +0900

    Adding changes

--- src/useKey.tsx
+++ src/useKey.tsx
@@ -4,10 +4,12 @@ export default function useKey(key: any, cb: any) {
     const callbackref = useRef(cb)
     useEffect(() => {
         callbackref.current = cb;
-    })
+        console.log(callbackref)
+    }, [])
     useEffect(() => {
         function handle(e: any) {
             if (e.code === key) {
+                console.log("calling function")
                 callbackref.current(e)
             }
         }
@@ -15,7 +17,6 @@ export default function useKey(key: any, cb: any) {
         return () => document.removeEventListener("keypress", handle)
     }, [key])
 }
-
 
 
 // https://www.youtube.com/watch?v=YxsfJoKJZ50 Thank you Bhargav
